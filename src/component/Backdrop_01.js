2537386d03a17a4e3b4b070bc158311b32c2ea71
209410801 <209410801@gms.tku.edu.tw>
2021-10-07 19:29:26 +0800

    add <Backdrop_01 />, <Modal_01 />

---
+++ src/component/Backdrop_01.js
@@ -0,0 +1,7 @@
+function Backdrop_01(props) {
+    return (
+       <div className="backdrop" onClick={props.onClose}></div>
+    )
+}
+
+export default Backdrop_01;
--- src/component/Todo_01.js
+++ src/component/Todo_01.js
@@ -1,12 +1,27 @@
+import { useState } from 'react';
+import Backdrop_01 from './Backdrop_01';
+import Modal_01 from './Modal_01';
+
 function Todo_01 (props) {
+    const [showModal, setShowModal] = useState(); //const [值, 方法] = useState(); //當useState資料被改過後，return會重新跑一次。
+
+    function showModalHandler() {
+        setShowModal(true);
+    }
+
+    function closeModalHandler() {
+        setShowModal(false);
+    }
     return (
         <div className="card">
             <h2>{props.text}</h2>
             <div className="actions">
-                <button className="btn">Delete</button>
+                <button className="btn" onClick={showModalHandler}>Delete</button>
             </div>
-       </div>
-    )
+            {showModal && <Backdrop_01 onClose={closeModalHandler}/>}
+            {showModal && <Modal_01 text='Are you sure?' onClose={closeModalHandler}/> }
+       </div>       
+    );
 }
 
 export default Todo_01;
